c968d095f3c305ac5c6dab678f6ef25ac8769fae
Marco Chan <marco.chan616@gmail.com>
2021-12-12 09:20:18 -0800

    testfiles

--- test/test_mips_cpu_harvard.sh
+++ test/test_mips_cpu_harvard.sh
@@ -4,8 +4,8 @@ set -eou pipefail
 source_directory="$1"
 
 instruction="${2:-all_cases}"
-echo "$source_directory"
-echo "$instruction"
+echo "Src Directory: $source_directory"
+echo "Instruction: $instruction"
 
 if [[ $instruction != "all_cases" ]]; then
     TESTCASES="test/${instruction}*"
@@ -14,41 +14,47 @@ if [[ $instruction != "all_cases" ]]; then
         TESTNAME=$(basename ${i} .v)
         echo "$TESTNAME"
 
+        #compile program
         iverilog -Wall -g 2012 \
-            -s test/$TESTNAME \
-            -o test/$TESTNAME test/$TESTNAME.v $i $1/mips_cpu_harvard.v $1/alucontrol.v $1/data_address_control.v $1/hilo.v $1/link.v $1/lw.v $1/pc_update.v $1/register_file.v $1/control.v $1/data_memory.v $1/instruction_memory.v $1/load.v $1/pc.v $1/unsign.v $1/tb.v $1/alu.v $1/branch_data.v $1/shift_control.v $1/sb.v
-    done
-fi
+            -s test/${TESTNAME} \
+            -o test/${TESTNAME} ${i} $1/mips_cpu_harvard.v $1/alucontrol.v $1/data_address_control.v $1/hilo.v $1/link.v $1/lw.v $1/pc_update.v $1/register_file.v $1/control.v $1/data_memory.v $1/instruction_memory.v $1/load.v $1/pc.v $1/unsign.v $1/alu.v $1/branch_data.v $1/shift_control.v $1/sb.v
 
-
-    set +e
-    iverilog -Wall -g 2012 \
-    -s  \
-    -o multiplier_iterative_tb \
-    multiplier_iterative_${VARIANT}.v multiplier_iterative_tb.v 
-    RESULT=$?
-    set -e
-    if [[ RESULT -ne 0 ]] ; then
-        echo "$i,Fail,Compilation"
-    
-    else
+        #run program    
         set +e
-        ./$TESTNAME
+        ./test/${TESTNAME}
         RESULT=$?
         set -e
         if [[ RESULT -eq 0 ]] ; then
-            echo "$i,Pass"
+            echo "${TESTNAME} ${instruction} Pass"
         else
-            echo "$i,Fail,Execution"
+            echo "${TESTNAME} ${instruction} Fail"
         fi
-    fi
+    done
+else
+    echo "Implement all instruction"
+    TESTCASES="test/*.v"
+    for i in ${TESTCASES} ; do
+        echo "$i"
+        TESTNAME=$(basename ${i} .v)
+        echo "$TESTNAME"
+        #compile program
+        iverilog -Wall -g 2012 \
+            -s test/${TESTNAME} \
+            -o test/${TESTNAME} ${i} $1/mips_cpu_harvard.v $1/alucontrol.v $1/data_address_control.v $1/hilo.v $1/link.v $1/lw.v $1/pc_update.v $1/register_file.v $1/control.v $1/data_memory.v $1/instruction_memory.v $1/load.v $1/pc.v $1/unsign.v $1/alu.v $1/branch_data.v $1/shift_control.v $1/sb.v
 
+        #run program    
+        set +e
+        ./test/${TESTNAME}
+        RESULT=$?
+        set -e
+        if [[ RESULT -eq 0 ]] ; then
+            echo "${TESTNAME} ${TESTNAME:: -1} Pass"
+        else
+            echo "${TESTNAME} ${TESTNAME:: -1} Fail"
+        fi
 
-    set +e
-    ./${i}
-    # Capture the exit code of the simulator in a variable
-    RESULT=$?
-    set -e
+        
+    done
 
-    
-done
+fi
+
