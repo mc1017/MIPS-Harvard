50cf45aeb4e2bb9f1821b309403cf1416e87a79d
jw38176 <93932554+jw38176@users.noreply.github.com>
2021-12-14 16:39:58 +0000

    Add files via upload

--- rtl/unsign.v
+++ rtl/unsign.v
@@ -4,7 +4,8 @@ module unsign(
 	); 
 	
 	always @(*) begin 
-		if((instruction[31:26] == 0) && ((instruction[5:0] == 6'b011001) || (instruction[5:0] == 6'b011011))) begin 
+		if(((instruction[31:26] == 0) && ((instruction[5:0] == 6'b011001) || (instruction[5:0] == 6'b011011))) ||
+			((instruction[31:26] == 6'b000001) || (instruction[31:26] == 6'b000111) || (instruction[31:26] == 6'b000110))) begin 
 			unsign = 0; 
 		end else begin 
 			unsign = 1; 
