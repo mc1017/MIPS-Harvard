38abcdb5649fe30f0748e1db782c776f74e21399
Marco Chan <marco.chan616@gmail.com>
2021-12-15 16:58:05 -0800

    all testcases passed

--- rtl/mips_cpu_sb.v
+++ rtl/mips_cpu_sb.v
@@ -6,8 +6,9 @@ module mips_cpu_sb(
 );
     always @(*) begin
         case (insop)
-            6'b101000: out={mem_data[31:8],reg_data[7:0]};
-            6'b101001: out={mem_data[31:16],reg_data[15:0]};
+            6'b101000: out={reg_data[7:0],mem_data[23:0]};
+            6'b101001: out={reg_data[7:0],reg_data[15:8],mem_data[15:0]};
+            6'b101011: out={reg_data[7:0],reg_data[15:8],reg_data[23:16],reg_data[31:24]};
         endcase
     end
 	
