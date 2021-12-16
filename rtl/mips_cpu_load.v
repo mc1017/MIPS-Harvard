7644d1b4f7977719594fa8b72e483a4d6593f122
Marco Chan <marco.chan616@gmail.com>
2021-12-15 16:18:32 -0800

    load fixed

--- rtl/mips_cpu_load.v
+++ rtl/mips_cpu_load.v
@@ -9,10 +9,10 @@ module mips_cpu_load(
         case (instruction[31:26])
             6'b100000: out={(read_data[31])? 24'hffffff:24'd0 ,read_data[31:24]}; //LB load byte          
             6'b100100: out={24'b0, read_data[31:24]};//LBU load byte unsigned
-            6'b100001: out={(read_data[31])? 16'hffff:16'd0,read_data[31:16]}; //LH load half-word
-            6'b100101: out={16'd0,read_data[31:16]};
+            6'b100001: out={(read_data[31])? 16'hffff:16'd0,read_data[23:16],read_data[31:24]}; //LH load half-word
+            6'b100101: out={16'd0,read_data[23:16],read_data[31:24]};
             6'b001111: out={instruction[15:0],16'd0};
-            6'b100011: out=read_data;
+            6'b100011: out={read_data[7:0],read_data[15:8],read_data[23:16],read_data[31:24]};
             default: out=read_data;
         endcase
         reg_data=out;
--- rtl/mips_cpu_lw.v
+++ rtl/mips_cpu_lw.v
@@ -11,17 +11,17 @@ module mips_cpu_lw(
         case (instruction[31:26])
             6'b100010: //load word left
                 case (mem_address%4)
-                    0: out=read_data;
-                    1:  out={read_data[23:0],reg_d[7:0]};
-                    2:  out={read_data[15:0],reg_d[15:0]};
+                    0: out={read_data[7:0],read_data[15:8],read_data[23:16],read_data[31:24]};
+                    1:  out={read_data[7:0],read_data[15:8],read_data[23:16],reg_d[7:0]};
+                    2:  out={read_data[7:0],read_data[15:8],reg_d[15:0]};
                     3:  out={read_data[7:0],reg_d[23:0]};
                 endcase
             6'b100110: //the address for the data is changed to the closest position where add%4=0
                 case (mem_address%4)
                     0:  out={reg_d[31:8],read_data[31:24]};
-                    1:  out={reg_d[31:16],read_data[31:16]};
-                    2:  out={reg_d[31:24],read_data[31:8]};
-                    3:  out=read_data;
+                    1:  out={reg_d[31:16],read_data[23:16],read_data[31:24]};
+                    2:  out={reg_d[31:24],read_data[15:8],read_data[23:16],read_data[31:24]};
+                    3:  out={read_data[7:0],read_data[15:8],read_data[23:16],read_data[31:24]};
                 endcase
             default: out=read_data;
 
